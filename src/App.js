c9c2adaec79b9862f7ed5cd796e5c28f5aed90c0
patriotsbreeze <68723574+patriotsbreeze@users.noreply.github.com>
2025-04-03 22:34:07 -0400

    beta-0.1
    - social media stuff

--- src/App.js
+++ src/App.js
@@ -22,32 +22,36 @@ function App() {
 
     return (
         <div className="App">
+            <title>Brendan Lo</title>
             {init && <Particles options={particlesOptions}/>}
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save
-                    to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
+                <h1>Brendan Lo</h1>
+                <div id = "socialMedia">
+                    <a href = "https://www.instagram.com/brendanloalt/" target = "_blank">
+                        <img src = "https://img.icons8.com/?size=100&id=43625&format=png&color=000000" width = "50"></img>
+                    </a>
+                    <a href = "https://discord.com/users/774759153893507123" target = "_blank">
+                        <img src = "https://img.icons8.com/?size=100&id=61604&format=png&color=000000" width = "50"></img>
+                    </a>
+                    <a href = "https://www.linkedin.com/in/brendan-lo-8b0b80247" target = "_blank">
+                        <img src = "https://img.icons8.com/?size=100&id=44019&format=png&color=000000" width = "50"></img>
+                    </a>
+                    <a href = "https://github.com/patriotsbreeze" target = "_blank">
+                        <img src = "https://img.icons8.com/?size=100&id=52539&format=png&color=000000" width = "50"></img>
+                    </a>
+                    <a href = "mailto:contact@brendanlo.xyz" target = "_blank">
+                        <img src = "https://img.icons8.com/?size=100&id=43999&format=png&color=000000" width = "50"></img>
+                    </a>
+                    <a href = "https://www.youtube.com/@brendan430" target = "_blank">
+                        <img src = "https://img.icons8.com/?size=100&id=44112&format=png&color=000000" width = "50"></img>
+                    </a>
+                    <a href = "https://open.spotify.com/user/1jqfcl3a716rdyg1gsi0rzc0v?si=f406480cd5cf4b8b" target = "_blank">
+                        <img src = "https://img.icons8.com/?size=100&id=49479&format=png&color=000000" width = "50"></img>
+                    </a>
+                </div>
+                <div>
+                    <p>Website is still in development.. check back later!!</p>
+                </div>
             </header>
         </div>
     );
